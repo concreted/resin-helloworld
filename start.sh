0ff89cce27188d3fe1b744b757b0b1958a850a0a
Aric Huang <arichuang@gmail.com>
2017-12-10 13:37:01 -0800

    rename wifi connect cmd

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 # Do wifi connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-./resin-wifi-connect --clear=true
+./wifi-connect --clear=true
 
 # Start application
 npm start
